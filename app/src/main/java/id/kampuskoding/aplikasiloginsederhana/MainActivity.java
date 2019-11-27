757caf9555e34dc175b7a2a6965136513eb2671d
AlikaNovitasari <alika.novitasari99@gmail.com>
2019-11-27 11:13:27 +0700

    android

---
+++ app/src/main/java/id/kampuskoding/aplikasiloginsederhana/MainActivity.java
@@ -0,0 +1,52 @@
+package id.kampuskoding.aplikasiloginsederhana;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usernameKey = username.getText().toString();
+                String passwordKey = password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                    //jika login berhasil
+                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Welcome.class);
+                    MainActivity.this.startActivity(intent);
+                    finish();
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password Anda salah!")
+                            .setNegativeButton("Retry", null).create().show();
+                }
+            }
+
+        });
+
+    }
+}
+
---
+++ app/src/main/java/id/kampuskoding/aplikasiloginsederhana/Welcome.java
@@ -0,0 +1,28 @@
+package id.kampuskoding.aplikasiloginsederhana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    Button btnExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perintah untuk mengakhiri aplikasi
+                finish();
+            }
+        });
+
+    }
+}
